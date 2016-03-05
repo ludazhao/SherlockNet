b07af9ce2d0fa135f1b03704df699459169a7e5c
Ubuntu <ubuntu@ip-172-31-8-98.us-west-1.compute.internal>
2016-03-05 23:25:43 +0000

    commit before pulling image_util.py

--- scripts/image_util.py
+++ scripts/image_util.py
@@ -3,5 +3,5 @@ import numpy
 from PIL import Image
 
 #Generate a number of different augmented images(flipped, cropped, scaled) at the same directory as the original image
-def gen_aug_imgs(num_imgs, file_path, flipped=True, crop_per=20, scale_per=20):
+def gen_aug_imgs(num_imgs, file_path, flipped=True, crop_per=20):
 
